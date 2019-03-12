c7029b66b511f9d57bf2a2f081c8e69de90dd7de
Robbie Smith <robbiesmith79@gmail.com>
2019-03-12 07:08:34 -0500

    Updated tests again

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
     $set2 = [
         [6,3,9,2,4,1,7,8,5],
-        [2,8,4,7,6,5,10,9,3],
+        [2,8,4,7,6,5,11,9,3],
         [5,1,7,9,8,3,6,2,4],
         [1,2,3,8,5,7,9,4,6],
         [7,9,6,4,3,2,8,5,1],
@@ -73,10 +73,7 @@
                 if ($number > 0 && $number < 10 && is_numeric($set[$row][$col])) {
                     // great!
                 } else {
-                    if (is_string($number) || is_null($number)) {
-                        return false; // just bad data
-                    }
-                    array_splice($numbers[$col],1);  // remove that square and reindex. Will use the count function later.
+                    return false; // just bad data any way you cut it.                   
                 }
             }
 
